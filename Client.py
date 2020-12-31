4d32251aea9032b3e3cd8e39edef914ebf12e38d
shayRok <shayrokach198@gmail.com>
2020-12-31 03:27:54 +0200

    comments

--- Client.py
+++ Client.py
@@ -3,29 +3,36 @@ import struct
 import msvcrt
 import time
 
+# the client chooses group name
 val = input("enter your team name: ")
-print(val)
+# print(val)
 team_name = val
+
 udpServerPort = 13117
 
+# create udp socket to listen to the broadcast
 print("Client started, listening for offer requests... ")
 UDPclientSocket = socket(AF_INET, SOCK_DGRAM)
 UDPclientSocket.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
 UDPclientSocket.bind(('', udpServerPort))
 
+# for 10 sec receive the broadcast and ask the server for tcp connection
 timer = time.time() +10
 while time.time() < timer:
-    # recive message from server
+    # recive broadcast from server
     data, serverAddress = UDPclientSocket.recvfrom(28)
     TCPclientSocket = socket(AF_INET, SOCK_STREAM)
+    # get and unpack the message and if its suitable to the format close udp socket and open tcp socket
     openMessage = struct.unpack('Ibh', data)
     if openMessage[0] == 0xfeedbeef and openMessage[1] == 0x2:
         tcpServerPort = openMessage[2]
         UDPclientSocket.close()
         TCPclientSocket.connect(("localhost", tcpServerPort))
         TCPclientSocket.send((team_name + '\n').encode())
+        # receive message from server - about the game starting
         firstMessage = TCPclientSocket.recv(1024).decode()
         print(firstMessage)
+        # for 10 sec send every key press
         timer = time.time() + 10
         while time.time() < timer:
             c = msvcrt.getch()
--- Server.py
+++ Server.py
@@ -11,12 +11,14 @@ group2 = []
 # the last cell - the teams counter - the number of chars sended
 
 def connectTCP():
+    # create tcp socket
     tcpServerPort = 2019
     tcpServerSocket = socket(AF_INET, SOCK_STREAM)
     tcpServerSocket.bind(("localhost", tcpServerPort))
     tcpServerSocket.settimeout(10)
     tcpServerSocket.listen(10)
 
+    # receuve messages for 10 sec on tcp and save for each group its data in suitable db
     timer = time.time() + 10
     while time.time() < timer:
         try:
@@ -36,12 +38,14 @@ def broadcast():
     udp_port = 13117
     tcp_port = 2019
 
+    # create udp socket
     udpServerSocket = socket(AF_INET, SOCK_DGRAM)
     udpServerSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
     udpServerSocket.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
     udpServerSocket.settimeout(0.2)
     print('Server started, listening on IP address ' + 'localhost'.format())
 
+    # the message to broadcast in udp connection- for 10 sec
     message = struct.pack('Ibh', 0xfeedbeef, 0x2, tcp_port)
     for i in range(10):
         udpServerSocket.sendto(message, ('<broadcast>', udp_port))
@@ -50,10 +54,11 @@ def broadcast():
 
 
 def threadForEachClient(socketPerClient, allMassage):
-    # each client will be treated in thread at the same ti,e as the others
+    # each client will be treated in thread at the same time as the others
     socketPerClient[1].send(bytes(allMassage, 'utf-8'))
     t = time.time() + 10
     while time.time() <= t:
+        # count key pressess of each client
         socketPerClient[1].recv(1024)
         socketPerClient[2] += 1
 
@@ -63,14 +68,15 @@ def main():
         udpServerPort = 13117
         tcpServerPort = 2019
 
-        t1 = threading.Thread(target=broadcast)
-        t2 = threading.Thread(target=connectTCP)
+        broadcastt1 = threading.Thread(target=broadcast)
+        connectTCPt2 = threading.Thread(target=connectTCP)
 
-        t1.start()  # broadcast announcements
-        t2.start()  # accepting clients and assigning groups
-        t1.join()
-        t2.join()
+        broadcastt1.start()  # broadcast udp thread
+        connectTCPt2.start()  # creating tcp connections with clients thread
+        broadcastt1.join()
+        connectTCPt2.join()
 
+        # the first message sent on tcp:
         startString = "Welcome to Keyboard Spamming Battle Royale.\n"
         group1String = "Group1:\n==\n"
         group2String = "Group2:\n==\n"
@@ -81,6 +87,7 @@ def main():
         endString = "\nStart pressing keys on your keyboard as fast as you can!!"
         allMassage = startString + group1String + group2String + endString
 
+        # sending the message to each client
         for socketPerClient in group1 + group2:
             t3 = threading.Thread(target=threadForEachClient, args=(socketPerClient, allMassage))
             t3.start()
@@ -89,12 +96,14 @@ def main():
         group1_score = 0
         group2_score = 0
 
+        # adding score to each group
         for socketPerClient in group1:
             group1_score += socketPerClient[2]
 
         for socketPerClient in group2:
             group2_score += socketPerClient[2]
 
+        # print the win message
         print("Game over!\nGroup 1 typed in "+ str(group1_score) +" characters. Group 2 typed in "+str(group2_score)+" characters.\n")
         if group1_score > group2_score:
             print("Group 1 wins!")
